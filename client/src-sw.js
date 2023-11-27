21d0b290e990b11e46ef8fa22429b238d69dc098
Yemny <y.feliz1296@gmail.com>
2023-11-27 17:05:57 -0500

    created registerRoute function so there's a route for root path of the app and the index.html

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+//register a route for the path of application and index.html.
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
